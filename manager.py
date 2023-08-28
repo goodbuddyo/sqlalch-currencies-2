8e93d7bf57f5841abddf29fcda3eb018510fbf3a
Seano <sean01234@yahoo.com>
2023-08-27 21:13:25 -0400

    copy over Portfolio & Investment model classes

--- manager.py
+++ manager.py
@@ -26,11 +26,38 @@ class Base(DeclarativeBase):
 
 
 class Portfolio(Base):
-    pass
+    __tablename__ = "portfolio"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(256))
+    description: Mapped[str] = mapped_column(Text())
+
+    investments: Mapped[List["Investment"]] = relationship(
+        back_populates="portfolio")
+
+    def __repr__(self) -> str:
+        return f"<Portfolio name: {self.name} with {len(self.investments)} investment(s)>"
 
 
 class Investment(Base):
-    pass
+    __tablename__ = "investment"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    coin: Mapped[str] = mapped_column(String(32))
+    currency: Mapped[str] = mapped_column(String(3))
+    amount: Mapped[float] = mapped_column(Numeric(5, 2))
+
+    portfolio_id: Mapped[int] = mapped_column(ForeignKey("portfolio.id"))
+    portfolio: Mapped["Portfolio"] = relationship(
+        back_populates="investments")
+
+    def __repr__(self) -> str:
+        return f"<Investment coin: {self.coin}, currency: {self.currency}, amount: {self.amount}>"
+
+
+# engine = create_engine("sqlite:///manager.db")
+engine = create_engine("postgresql://postgres:pgpassword@localhost/manager")
+Base.metadata.create_all(engine)
 
 
 @click.group()
